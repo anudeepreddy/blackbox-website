468f15d85af612221c48bcb8667b823c59635164
Anudeep Reddy <anudeepc85@gmail.com>
2025-09-13 21:57:39 +0530

    added umami

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { GeistSans } from "geist/font/sans"
 import { GeistMono } from "geist/font/mono"
 import { Suspense } from "react"
 import { ThemeProvider } from "@/components/theme-provider"
+import Script from 'next/script'
 import "./globals.css"
 
 export const metadata: Metadata = {
@@ -22,6 +23,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://umami.anudeepreddy.dev/script.js"
+          data-website-id="8239a00a-5374-4bfe-8997-fa00bd6b2598"
+          strategy="afterInteractive"
+        />
+      </head>
       <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
           <Suspense fallback={null}>{children}</Suspense>
